b4c1a1834dff072156e3b9fe3f31d2acd9836ebd
drawrs <rizal.hilman67@gmail.com>
2024-04-27 23:52:56 +0700

    Initial Commit

---
+++ Expenses Tracker App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expenses Tracker App
+//
+//  Created by Rizal Hilman on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expenses Tracker App/Expenses_Tracker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_Tracker_AppApp.swift
+//  Expenses Tracker App
+//
+//  Created by Rizal Hilman on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_Tracker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
